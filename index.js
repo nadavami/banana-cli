0283236e977fe58320feb3d876eb97ba50b545af
Nadav Ami <x@nadav.ca>
2018-06-22 15:53:53 -0400

    Parse swagger file from cli argument, exit with error if invalid

---
+++ index.js
@@ -0,0 +1,9 @@
+const SwaggerParser = require('swagger-parser')
+let args = process.argv.slice(2)
+
+SwaggerParser.validate(args[0])
+  .then(api => console.log(api))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
---
+++ tests/index.test.js
@@ -0,0 +1,14 @@
+const util = require('util')
+const execFile = util.promisify(require('child_process').execFile)
+
+describe('Run CLI tool', () => {
+  test('Should parse swagger file from first cli argument', async () => {
+    const {stdout, stderr} = await execFile('node', ['index.js', 'tests/swagger-example.yml'])
+    expect(stdout).toContain(/petstore.swagger.io/)
+    expect(stderr).toBe('')
+  })
+
+  test('Should print error when an invalid file is passed', async () => {
+    await expect(execFile('node', ['index.js', 'not-a-file'])).rejects.toThrowError(/no such file/)
+  })
+})
